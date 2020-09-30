e57679c867f4e7657e4af3ee29d8067be975b227
Svetlana <gubinas83@gmail.com>
2020-09-30 16:36:55 +0300

    feat: implement repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,38 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  if (str === null) {
+    str = "null";
+  }
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (typeof str !== String) {
+    str = str.toString();
+  }
+
+  let separator = options.separator || "+";
+  let additionSeparator = options.additionSeparator || "";
+
+  if (options.repeatTimes == 0 && options.additionRepeatTimes == 0) {
+    return str;
+  }
+
+  let addition;
+  let keys = Object.keys(options);
+
+  if (keys.includes('addition') && options.addition === null) {
+    addition = "null";
+  } else if (keys.includes('addition') && typeof options.addition !== String) {
+    addition = options.addition.toString();
+  } else {
+    addition = "";
+  }
+
+  let fullAddition = `${addition}${additionSeparator}`;
+
+  if (addition !== "") {
+    let fullAdditionRepeat = fullAddition.repeat(options.additionRepeatTimes - 1) + options.addition;
+    let construction = `${str}${fullAdditionRepeat}`;
+    let fullConstruction = `${str}${fullAdditionRepeat}${separator}`;
+    return fullConstruction.repeat(options.repeatTimes - 1) + construction;
+  } else {
+    return `${str}${separator}`.repeat(options.repeatTimes - 1).concat(str);
+  }
 };
-  
