ecaee21763c89f56a85c0c5b1180e1dce737593b
Svetlana <gubinas83@gmail.com>
2020-10-09 01:24:16 +0300

    feat: implement VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,8 +1,8 @@
-// const CustomError = require("../extensions/custom-error");
+const NUMBER_OF_LETTERS = 26;
 
 class VigenereCipheringMachine {
   constructor(type) {
-    this.type = type === false ? reverse : direct;
+    this.type = type === false ? 'reverse' : 'direct';
   }
 
   encrypt(message, key) {
@@ -14,13 +14,18 @@ class VigenereCipheringMachine {
       throw new TypeError("Message & key not transferred")
     }
 
-    let messageUpperCase = message.toUpperCase();
+    let messageUpperCase = this.type === 'direct' ? message.toUpperCase() : message.toUpperCase().split('').reverse().join('');
     let keyUppercase = key.toUpperCase();
 
     let strFromKey;
     if (messageUpperCase.length > keyUppercase.length) {
       let index = Math.ceil(messageUpperCase.length / keyUppercase.length);
       strFromKey = keyUppercase.repeat(index).slice(0, messageUpperCase.length);
+    } else if (messageUpperCase.length < keyUppercase.length) {
+      strFromKey = keyUppercase.slice(0, messageUpperCase.length);
+      console.log(strFromKey);
+    } else {
+      strFromKey = keyUppercase;
     }
 
     let testArr = messageUpperCase.split('');
@@ -39,8 +44,10 @@ class VigenereCipheringMachine {
         result += testArr[i];
       }
     }
+
     return result;
   }
+
   decrypt(encryptedMessage, key) {
     if (arguments.length === 0) {
       throw new TypeError("Message & key not transferred")
@@ -49,7 +56,39 @@ class VigenereCipheringMachine {
     if (!encryptedMessage || !key) {
       throw new TypeError("Message & key not transferred")
     }
-    // return this.type === direct ? str.toUpperCase() : str.reverse().toUpperCase();
+
+    let encryptedMessageUpperCase = this.type === 'direct' ? encryptedMessage.toUpperCase() : encryptedMessage.toUpperCase().split('').reverse().join('');
+    let keyUppercase = key.toUpperCase();
+
+    let strFromKey;
+    if (encryptedMessageUpperCase.length > keyUppercase.length) {
+      let index = Math.ceil(encryptedMessageUpperCase.length / keyUppercase.length);
+      strFromKey = keyUppercase.repeat(index).slice(0, encryptedMessageUpperCase.length);
+    } else if (encryptedMessageUpperCase.length < keyUppercase.length) {
+      strFromKey = keyUppercase.slice(0, encryptedMessageUpperCase.length);
+      console.log(strFromKey);
+    } else {
+      strFromKey = keyUppercase;
+    }
+
+    let testArr = encryptedMessageUpperCase.split('');
+    let keycCounter = 0;
+    let result = '';
+
+    for (let i = 0; i < testArr.length; i++) {
+      if (encryptedMessageUpperCase.charCodeAt(i) > 64 && encryptedMessageUpperCase.charCodeAt(i) < 91) {
+        let first = encryptedMessageUpperCase.charCodeAt(i) - 65;
+        let second = strFromKey.charCodeAt(keycCounter) - 65;
+        let sum = first + NUMBER_OF_LETTERS - second;
+        let res = (sum % NUMBER_OF_LETTERS) + 65;
+        result += String.fromCharCode(res);
+        keycCounter++;
+      } else {
+        result += testArr[i];
+      }
+    }
+
+    return result;
   }
 }
 
